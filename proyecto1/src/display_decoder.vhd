136d88fa9735c75b4249439c194b270c66c16665
Roly Sandro Gutierrez Benito <56467680+rsgbArch@users.noreply.github.com>
2025-02-21 05:39:39 -0500

    Update display_decoder.vhd

--- proyecto1/src/display_decoder.vhd
+++ proyecto1/src/display_decoder.vhd
@@ -1,9 +1,10 @@
+-- Hace la decodificación de la entrada para el display de 7 segmentos. 
 library IEEE;
 use IEEE.STD_LOGIC_1164.all;
 use ieee.std_logic_unsigned.all;
+
 entity display_decoder is
-  port
-  (
+  port(
     distance_in1 : in std_logic_vector (8 downto 0);
     display_sal  : out std_logic_vector (6 downto 0);
     display_sal2 : out std_logic_vector (6 downto 0)
